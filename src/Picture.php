ad551ea0665d2eed17783ba57aac1f058c1eb875
Pascal Schneider <psc@brandcom.de>
2022-03-15 16:33:20 +0100

    Set fields for Picture https://docs.silverstripe.org/en/4/developer_guides/model/data_types_and_casting/#overloading

--- src/Picture.php
+++ src/Picture.php
@@ -4,6 +4,8 @@ namespace jbennecker\Webp;
 
 use SilverStripe\Assets\Image;
 use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\CustomMethods;
+use SilverStripe\Dev\Debug;
 use SilverStripe\View\ViewableData;
 
 class Picture extends ViewableData
@@ -12,6 +14,12 @@ class Picture extends ViewableData
 
     private Image $image;
 
+    private string $test;
+
+    private static $casting = [
+        'forTemplate' => 'HtmlText',
+    ];
+
     public function __construct(Image $image)
     {
         parent::__construct();
@@ -19,10 +27,23 @@ class Picture extends ViewableData
         $config = self::config();
 
         $this->image = $image;
+        $this->test = "123";
     }
 
     public function getTest(): string
     {
         return "Test";
+    }
+
+    public function setTest(string $test="default"): Picture
+    {
+        $this->test = $test;
+        Debug::dump($this->test);
+        return $this;
+    }
+
+    public function forTemplate(): string
+    {
+        return '<span style="color: tomato;">' . $this->test . '</span>';
     }
 }
